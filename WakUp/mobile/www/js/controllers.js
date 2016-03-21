4c3473868c8d9dd2f83ce296d8da58e005f5e58c
Stephane Garnier <stephane.garnier@wakanda.io>
2016-03-21 15:43:38 +0100

    first commit

---
+++ WakUp/mobile/www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('createAccountCtrl', function($scope) {
+
+})
+   
+.controller('profileCtrl', function($scope) {
+
+})
+   
+.controller('editProfileCtrl', function($scope) {
+
+})
+ 
---
+++ WakUp/mobile/www/js/routes.js
@@ -0,0 +1,41 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('login', {
+    url: '/page1',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('createAccount', {
+    url: '/page3',
+    templateUrl: 'templates/createAccount.html',
+    controller: 'createAccountCtrl'
+  })
+
+  .state('profile', {
+    url: '/page4',
+    templateUrl: 'templates/profile.html',
+    controller: 'profileCtrl'
+  })
+
+  .state('editProfile', {
+    url: '/page5',
+    templateUrl: 'templates/editProfile.html',
+    controller: 'editProfileCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1')
+
+  
+
+});
